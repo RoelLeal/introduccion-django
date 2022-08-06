092ca9fd70e0a77b9563529f8488ded3ef3d3cc9
Shark11killer <vegerex91@gmail.com>
2022-08-06 12:35:57 -0500

    Crear POST

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import blogListView
+from .views import blogListView, blogCreateView
 
 app_name = "blog"
 
 urlpatterns = [
-      path('', blogListView.as_view(), name="home")
+      path('', blogListView.as_view(), name="home"),
+      path('create/', blogCreateView.as_view(), name="create")
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, redirect
 from django.views.generic.base import View
+from .forms import PostCreateForm
+from .models import Post
 
 class blogListView(View):
       def get(self, request, *args, **kwargs):
@@ -7,3 +9,25 @@ class blogListView(View):
                   
             }
             return render(request, 'blog_list.html', context)
+
+class blogCreateView(View):
+      def get(self, request, *args, **kwargs):
+            form = PostCreateForm()
+            context = {
+                'form': form  
+            }
+            return render(request, 'blog_create.html', context)
+      def post(self, request, *args, **kwargs):
+            if request.method == "POST":
+                  form = PostCreateForm(request.POST)
+                  if form.is_valid():
+                        title = form.cleaned_data.get('title')
+                        content = form.cleaned_data.get('content')
+
+                        p, created = Post.objects.get_or_create(title = title, content = content)
+                        p.save()
+                        return redirect('blog:home')
+            context = {
+                  
+            }
+            return render(request, 'blog_create.html', context)      
